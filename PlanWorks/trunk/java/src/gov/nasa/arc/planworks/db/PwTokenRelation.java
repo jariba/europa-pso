767b9708f5e318d3193e3c87a56b0f98f4dad892
taylor <taylor@23e1bc29-d954-0410-9766-47de34158ba0>
2003-05-16 18:33:41 +0000

    *** empty log message ***

---
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/db/PwTokenRelation.java
@@ -0,0 +1,30 @@
+// 
+// * See the file "PlanWorks/disclaimers-and-notices.txt" for 
+// * information on usage and redistribution of this file, 
+// * and for a DISCLAIMER OF ALL WARRANTIES. 
+// 
+
+// $Id: PwTokenRelation.java,v 1.1 2003-05-16 18:33:41 taylor Exp $
+//
+// PlanWorks -- 
+//
+// Will Taylor -- started 16May03
+//
+
+package gov.nasa.arc.planworks.db;
+
+import java.util.List;
+
+
+/**
+ * <code>PwTokenRelation</code> - 
+ *
+ * @author <a href="mailto:william.m.taylor@nasa.gov">Will Taylor</a>
+ *                         NASA Ames Research Center - Code IC
+ * @version 0.0
+ */
+public interface PwTokenRelation {
+
+
+
+} // end interface PwTokenRelation
