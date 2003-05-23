6d973958f20e901f6bc3068c2c455752dd9bf262
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2003-05-23 19:28:18 +0000

    Got test working.  ExitException status 0 means System.exit(0) got called.

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/views/test/TimelineViewTest.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/views/test/TimelineViewTest.java
@@ -3,54 +3,51 @@ package gov.nasa.arc.planworks.viz.views.test;
 import java.awt.Color;
 import java.util.Set;
 import junit.extensions.jfcunit.*;
+import junit.framework.*;
+import junit.textui.TestRunner;
 import gov.nasa.arc.planworks.proj.test.PwProjectTest;
 
-public class TimelineViewTest extends JFCTestCase
-{
-		private JFCTestHelper helper;
-		
-		public TimelineViewTest(String test)
-		{
-				super(test);
-		}
+public class TimelineViewTest extends JFCTestCase {
 
-		public void setUp()
-		{
-				helper = new JFCTestHelper();
-		}
-		
-		public void tearDown() throws Exception
-		{
-				helper.cleanUp(this);
-				super.tearDown();
-		}
-
-                public void testMain()
-		{
-				Set windows;
-
-				awtSleep();
-				windows = helper.getWindows();
-				assertEquals("Timeline window failed to open", 1, windows.size());
-		}
-
-                public static void main( String[] args)
-                {
-                                boolean jvm_ge_14 = false;
-				if(System.getProperty("java.version").compareTo("1.4.1") >= 0)
-						jvm_ge_14 = true;
-				PwProjectTest projectTest =
-                                  new PwProjectTest( System.getProperty("os.name"),
-                                                     jvm_ge_14,
-                                                     System.getProperty("xml.files.dir"));
-                                projectTest.setSize( PwProjectTest.FRAME_WIDTH,
-                                                     PwProjectTest.FRAME_HEIGHT);
-                                projectTest.setLocation( PwProjectTest.FRAME_X_LOCATION,
-                                                         PwProjectTest.FRAME_Y_LOCATION);
-                                projectTest.setBackground( Color.gray);
-                                projectTest.setVisible( true);
-
+  private JFCTestHelper helper;
+  
+  public TimelineViewTest(String test) {
+    super(test);
+  }
+  
+  public void setUp() {
+    helper = new JFCTestHelper();
+  }
+  
+  public void tearDown() throws Exception {
+    helper.cleanUp(this);
+    super.tearDown();
+  }
+  
+  public void testMain() {
+    Set windows;
+    
+    awtSleep();
+    windows = helper.getWindows();
+    assertEquals("Timeline window failed to open", 1, windows.size());
+  }
+  
+  public static void main( String[] args) {
+    boolean jvm_ge_14 = false;
+    if(System.getProperty("java.version").compareTo("1.4.1") >= 0)
+      jvm_ge_14 = true;
+    PwProjectTest projectTest =
+      new PwProjectTest( System.getProperty("os.name"),
+                         jvm_ge_14,
+                         System.getProperty("xml.files.dir"));
+    projectTest.setSize( PwProjectTest.FRAME_WIDTH,
+                         PwProjectTest.FRAME_HEIGHT);
+    projectTest.setLocation( PwProjectTest.FRAME_X_LOCATION,
+                             PwProjectTest.FRAME_Y_LOCATION);
+    projectTest.setBackground( Color.gray);
+    projectTest.setVisible( true);
+    TestRunner.run(TimelineViewTest.class);
                                 // testMain();
-                }
+  }
 }
  
