3aac40a68ef276f65bd5316df8057b72ca1c9b95
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2003-05-28 01:00:39 +0000

    First run of basic JGo tests.  Only checks valid monkey timeline names for now.

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/views/test/TimelineViewTest.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/views/test/TimelineViewTest.java
@@ -1,16 +1,28 @@
 package gov.nasa.arc.planworks.viz.views.test;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Set;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import junit.extensions.jfcunit.*;
+import junit.extensions.jfcunit.eventdata.EventDataConstants;
+import junit.extensions.jfcunit.eventdata.MouseEventData;
 import junit.framework.*;
 import junit.textui.TestRunner;
 import gov.nasa.arc.planworks.proj.test.PwProjectTest;
+import gov.nasa.arc.planworks.viz.views.timeline.TimelineView;
+import gov.nasa.arc.planworks.viz.nodes.TimelineNode;
+import gov.nasa.arc.planworks.viz.nodes.SlotNode;
 
 public class TimelineViewTest extends JFCTestCase {
 
   private JFCTestHelper helper;
-  
+
   public TimelineViewTest(String test) {
     super(test);
   }
@@ -22,30 +34,54 @@ public class TimelineViewTest extends JFCTestCase {
   public void tearDown() throws Exception {
     helper.cleanUp(this);
     super.tearDown();
+    System.exit(0);
   }
   
-  public void testMain() {
-    Set windows;
-    
+  public void testMain() throws Exception {
     awtSleep();
-    windows = helper.getWindows();
+    Set windows = helper.getWindows();
     assertEquals("Timeline window failed to open", 1, windows.size());
+    JFrame frame = (JFrame)(windows.toArray())[0];
+    assertNotNull("Failed to get frame from set", frame);
+    JMenuBar menuBar = frame.getJMenuBar();
+    assertNotNull("Failed to get menu bar from frame", menuBar);
+    JMenu renderMenu = (JMenu) menuBar.getComponent(1);
+    assertNotNull("Failed to get menu.", renderMenu);
+    assertTrue("Failed to get \"Render\" menu.", renderMenu.getText().equals("Render"));
+    JMenuItem renderItem = renderMenu.getItem(0);
+    assertTrue("Failed to get \"Timeline View\" item.", 
+               renderItem.getText().equals("Timeline View"));
+    helper.enterClickAndLeave(new MouseEventData(this, renderMenu));
+    helper.enterClickAndLeave(new MouseEventData(this, renderItem));
+    //helper.mousePressed(renderItem, EventDataConstants.DEFAULT_MOUSE_MODIFIERS, 1, false);
+    Container contentPane = frame.getContentPane();
+    //System.out.println(contentPane.getComponent(0));
+    TimelineView timelineView = (TimelineView) contentPane.getComponent(0);
+    assertNotNull("Failed to get timeline view.", timelineView);
+    List timelineNodes = timelineView.getTimelineNodeList();
+    ListIterator timelineNodeIterator = timelineNodes.listIterator();
+    while(timelineNodeIterator.hasNext()){
+        TimelineNode timelineNode = (TimelineNode) timelineNodeIterator.next();
+        String timelineName = timelineNode.getTimelineName();
+        List slotNodes = timelineNode.getSlotNodeList();
+        ListIterator slotNodeIterator = slotNodes.listIterator();
+        if(timelineName.indexOf("Monkey1 : LOCATION_SV") == -1 &&
+           timelineName.indexOf("Monkey1 : ALTITUDE_SV") == -1 &&
+           timelineName.indexOf("Monkey1 : BANANA_SV") == -1){
+          assertTrue("Invalid timeline name", false);
+        }
+    }
   }
   
   public static void main( String[] args) {
-    boolean jvm_ge_14 = false;
-    if(System.getProperty("java.version").compareTo("1.4.1") >= 0)
-      jvm_ge_14 = true;
-    PwProjectTest projectTest =
-      new PwProjectTest( System.getProperty("os.name"),
-                         jvm_ge_14,
-                         System.getProperty("xml.files.dir"));
-    projectTest.setSize( PwProjectTest.FRAME_WIDTH,
-                         PwProjectTest.FRAME_HEIGHT);
-    projectTest.setLocation( PwProjectTest.FRAME_X_LOCATION,
-                             PwProjectTest.FRAME_Y_LOCATION);
-    projectTest.setBackground( Color.gray);
-    projectTest.setVisible( true);
+    PwProjectTest projectTest = new PwProjectTest(System.getProperty("os.name"),
+                                                  System.getProperty("xml.files.dir"));
+    projectTest.setSize(PwProjectTest.FRAME_WIDTH,
+                        PwProjectTest.FRAME_HEIGHT);
+    projectTest.setLocation(PwProjectTest.FRAME_X_LOCATION,
+                            PwProjectTest.FRAME_Y_LOCATION);
+    projectTest.setBackground(Color.gray);
+    projectTest.setVisible(true);
     TestRunner.run(TimelineViewTest.class);
                                 // testMain();
   }
