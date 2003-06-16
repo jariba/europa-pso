0c4ba396f56020c9e06d4bfaaec6bba1c4268c5b
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2003-06-16 22:32:15 +0000

    *** empty log message ***

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/mdi/MDIDesktopPane.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/mdi/MDIDesktopPane.java
@@ -1,10 +1,18 @@
+//
+// * See the file "PlanWorks/disclaimers-and-notices.txt" for
+// * information on usage and redistribution of this file,
+// * and for a DISCLAIMER OF ALL WARRANTIES.
+//
+
+// $Id: MDIDesktopPane.java,v 1.3 2003-06-16 22:32:14 miatauro Exp $
+//
 // 
 // * See the file "PlanWorks/disclaimers-and-notices.txt" for 
 // * information on usage and redistribution of this file, 
 // * and for a DISCLAIMER OF ALL WARRANTIES. 
 // 
 
-// $Id: MDIDesktopPane.java,v 1.2 2003-06-13 20:22:27 miatauro Exp $
+// $Id: MDIDesktopPane.java,v 1.3 2003-06-16 22:32:14 miatauro Exp $
 //
 package gov.nasa.arc.planworks.mdi;
 
@@ -16,7 +24,8 @@ import javax.swing.UIManager;
  * <code>MDIDesktopPane</code> - was internal class of MDIDesktopFrame
  *          make it public class so that background color, etc can be
  *          changed
- *
+ * Sets OUTLINE_DRAG_MODE and the EmptyDesktopIconUI class so that iconifying (minimizing)
+ * an MDIInternalFrame doesn't create an icon at the bottom of the screen.
  * @author <a href="mailto:william.m.taylor@nasa.gov">Will Taylor</a>
  *                  NASA Ames Research Center - Code IC
  * @version 1.4
