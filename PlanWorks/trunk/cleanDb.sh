3c9c7851b3d291e730ed31a5274421a8f6062c27
taylor <taylor@23e1bc29-d954-0410-9766-47de34158ba0>
2003-07-07 18:50:37 +0000

    *** empty log message ***

--- PlanWorks/trunk/cleanDb.sh
+++ PlanWorks/trunk/cleanDb.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-eval "$1/mysqld --basedir=$2 --skip-bdb --bind-address=127.0.0.1 --datadir=$3 --log=$4 --log-error=$5 --skip-symlink --socket=$6 --tmpdir=$7 &"
+eval "$1/mysqld --basedir=$2 --skip-bdb --datadir=$3 --log=$4 --log-error=$5 --skip-symlink --socket=$6 --tmpdir=$7 &"
 
-eval "$1/mysql --user=PlanWorksUser --password=PlanWorksUser --host=127.0.0.1 --socket=$6 < cleandb"
+sleep 1
 
-eval "$1/mysqladmin --user=PlanWorksUser --password=PlanWorksUser --host=127.0.0.1 --socket=$6 shutdown"
+eval "$1/mysql --user=root --password=root --socket=$6 < cleandb"
+
+eval "$1/mysqladmin --user=root --password=root --socket=$6 shutdown"
