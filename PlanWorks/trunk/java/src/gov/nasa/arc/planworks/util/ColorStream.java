7f4141364e9d144a6a149de058b48815d093579c
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2003-09-16 18:38:25 +0000

    *** empty log message ***

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/util/ColorStream.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/util/ColorStream.java
@@ -1,14 +1,17 @@
 package gov.nasa.arc.planworks.util;
 
 import java.awt.Color;
-
-//temporary
-import java.util.Random;
+import java.util.ArrayList;
 
 public class ColorStream {
-  Random random;
+  private static final int COLOR_INC = 51;
+  private int r, g, b;
+  private List usedColors;
   public ColorStream() {
-    random = new Random();
+    usedColors = new ArrayList();
+    r = 255;
+    g = 153;
+    b = 204;
   }
   public ColorStream(int r, int g, int b) {
     this();
@@ -17,13 +20,35 @@ public class ColorStream {
     this();
   }
   public Color nextColor() {
-    return new Color(random.nextInt() % 256, random.nextInt() % 256, random.nextInt() % 256);
+    Color retval = new Color(r, g, b);
+    usedColors.add(retval);
+    r += COLOR_INC;
+    if(r > 255) {
+      r %= 255;
+      g += COLOR_INC;
+    }
+    if(g > 255) {
+      g %= 255;
+      b += COLOR_INC;
+    }
+    if(b > 255) {
+      b %= 255;
+      r = 0;
+      g = 0;
+    }
+    return retval;
   }
   public Color getColor(int index) {
-    return new Color(random.nextInt() % 256, random.nextInt() % 256, random.nextInt() % 256);
+    return (Color) usedColors.get(index);
   }
   public void setColor(Color color) {
+    r = color.getRed();
+    g = color.getGreen();
+    b = color.getBlue();
   }
   public void setColor(int r, int g, int b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
   }
 }
