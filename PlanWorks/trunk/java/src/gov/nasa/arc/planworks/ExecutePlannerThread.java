e51aa5d98f0cb59c49bca6a6090539c0b7007db0
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2003-12-05 00:33:28 +0000

    *** empty log message ***

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/ExecutePlannerThread.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/ExecutePlannerThread.java
@@ -1,16 +1,31 @@
 package gov.nasa.arc.planworks;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
 public class ExecutePlannerThread extends Thread {
   private String commandLine;
-  public ExecutePlannerThread(String commandLine) {
+  private String [] env;
+  private static final String ENV_DEST = "PPW_WRITE_DEST";
+  private static final String ENV_NSTEPS = "PPW_WRITE_NSTEPS";
+  public ExecutePlannerThread(String commandLine, String dest, String stepsPerWrite) {
     this.commandLine = commandLine;
+    env = new String[2];
+    env[0] = ENV_DEST + "=" + dest;
+    env[1] = ENV_NSTEPS + "=" + stepsPerWrite;
   }
   public void run() {
     try {
       System.err.println("Executing command '" + commandLine + "'");
       long t1 = System.currentTimeMillis();
-      Runtime.getRuntime().exec(commandLine);
-      System.err.println("Done.  Execution took " + (System.currentTimeMillis() - t1) + "ms");
+      Process planner = Runtime.getRuntime().exec(commandLine, env);
+      
+      InputStream is = planner.getInputStream();
+      int character;
+      while((character = is.read()) != -1) {
+        System.err.write(character);
+      }
+      System.err.println("Done.  Returned value " + planner.exitValue() + ".\nExecution took " + (System.currentTimeMillis() - t1) + "ms");
     }
     catch(Exception e){}
   }
