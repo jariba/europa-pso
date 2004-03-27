3c75944f6e8c5d6b8a84cd6a9801072f5f552d0b
taylor <taylor@23e1bc29-d954-0410-9766-47de34158ba0>
2004-03-27 00:53:52 +0000

    *** empty log message ***

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/partialPlan/tokenNetwork/TokenNetworkLayout.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/viz/partialPlan/tokenNetwork/TokenNetworkLayout.java
@@ -4,7 +4,7 @@
 // * and for a DISCLAIMER OF ALL WARRANTIES. 
 // 
 
-// $Id: TokenNetworkLayout.java,v 1.4 2004-03-27 00:30:46 taylor Exp $
+// $Id: TokenNetworkLayout.java,v 1.5 2004-03-27 00:53:52 taylor Exp $
 //
 // PlanWorks -- 
 //
@@ -30,19 +30,19 @@ import com.nwoods.jgo.layout.JGoLayeredDigraphAutoLayout;
 import com.nwoods.jgo.layout.JGoNetworkLink;
 import com.nwoods.jgo.layout.JGoNetworkNode;
 
-import org.jgraph.JGraph;
-import org.jgraph.graph.AbstractCellView;
-import org.jgraph.graph.CellHandle;
-import org.jgraph.graph.CellMapper;
-import org.jgraph.graph.CellView;
-import org.jgraph.graph.CellViewRenderer;
-import org.jgraph.graph.ConnectionSet;
-import org.jgraph.graph.GraphContext;
-import org.jgraph.graph.GraphLayoutCache;
-import org.jgraph.graph.GraphModel;
-import org.jgraph.graph.ParentMap;
-import org.jgraph.graph.ParentMap.Entry;
-import org.jgraph.layout.RadialTreeLayoutAlgorithm;
+// import org.jgraph.JGraph;
+// import org.jgraph.graph.AbstractCellView;
+// import org.jgraph.graph.CellHandle;
+// import org.jgraph.graph.CellMapper;
+// import org.jgraph.graph.CellView;
+// import org.jgraph.graph.CellViewRenderer;
+// import org.jgraph.graph.ConnectionSet;
+// import org.jgraph.graph.GraphContext;
+// import org.jgraph.graph.GraphLayoutCache;
+// import org.jgraph.graph.GraphModel;
+// import org.jgraph.graph.ParentMap;
+// import org.jgraph.graph.ParentMap.Entry;
+// import org.jgraph.layout.RadialTreeLayoutAlgorithm;
 
 import gov.nasa.arc.planworks.viz.nodes.TokenNode;
 
@@ -56,11 +56,12 @@ import gov.nasa.arc.planworks.viz.nodes.TokenNode;
  */
 public class TokenNetworkLayout extends JGoLayeredDigraphAutoLayout {
 
-  private static final String KEY_WIDTH = "Width";
-  private static final String KEY_HEIGHT = "Height";
+//   private static final String KEY_WIDTH = "Width";
+//   private static final String KEY_HEIGHT = "Height";
 
-  private Object[] rootNodes;
-  private ParentMap parentMap;
+//   private Object[] rootNodes;
+//   private ParentMap parentMap;
+
   private long startTimeMSecs;
 
 
@@ -95,30 +96,30 @@ public class TokenNetworkLayout extends JGoLayeredDigraphAutoLayout {
    * @param parentMap - <code>ParentMap</code> - 
    * @param startTimeMSecs - <code>long</code> - 
    */
-  public TokenNetworkLayout( JGoDocument jGoDocument, Object[] rootNodes,
-                             ParentMap parentMap, long startTimeMSecs) {
-    super( jGoDocument);
-    this.rootNodes = rootNodes;
-    this.parentMap = parentMap;
-    this.startTimeMSecs = startTimeMSecs;
-    setDirectionOption( JGoLayeredDigraphAutoLayout.LD_DIRECTION_DOWN);
-    setColumnSpacing( getColumnSpacing() / 4);
-    setLayerSpacing( getLayerSpacing() / 4);
-    setCycleRemoveOption( JGoLayeredDigraphAutoLayout.LD_CYCLEREMOVE_DFS);
-    // setInitializeOption( JGoLayeredDigraphAutoLayout.LD_INITIALIZE_NAIVE);
-    setInitializeOption( JGoLayeredDigraphAutoLayout.LD_INITIALIZE_DFSIN);
-    setLayeringOption( JGoLayeredDigraphAutoLayout.LD_LAYERING_OPTIMALLINKLENGTH);
+//   public TokenNetworkLayout( JGoDocument jGoDocument, Object[] rootNodes,
+//                              ParentMap parentMap, long startTimeMSecs) {
+//     super( jGoDocument);
+//     this.rootNodes = rootNodes;
+//     this.parentMap = parentMap;
+//     this.startTimeMSecs = startTimeMSecs;
+//     setDirectionOption( JGoLayeredDigraphAutoLayout.LD_DIRECTION_DOWN);
+//     setColumnSpacing( getColumnSpacing() / 4);
+//     setLayerSpacing( getLayerSpacing() / 4);
+//     setCycleRemoveOption( JGoLayeredDigraphAutoLayout.LD_CYCLEREMOVE_DFS);
+//     // setInitializeOption( JGoLayeredDigraphAutoLayout.LD_INITIALIZE_NAIVE);
+//     setInitializeOption( JGoLayeredDigraphAutoLayout.LD_INITIALIZE_DFSIN);
+//     setLayeringOption( JGoLayeredDigraphAutoLayout.LD_LAYERING_OPTIMALLINKLENGTH);
     
-    // int NlayerSpacing, int NcolumnSpacing, int NdirectionOption, int NcycleremoveOption,
-    //   int NlayeringOption, int NinitializeOption, int Niterations, int NaggressiveOption) 
+//     // int NlayerSpacing, int NcolumnSpacing, int NdirectionOption, int NcycleremoveOption,
+//     //   int NlayeringOption, int NinitializeOption, int Niterations, int NaggressiveOption) 
 
-  } // end constructor
+//   } // end constructor
 
 
-  // protected void layoutNodesAndLinks() {
-  protected void layoutNodesAndLinksNotUsed() {
-    JGoNetworkNode[] nodeArray = getNetwork().getNodeArray();
-    JGoNetworkLink[] linkArray = getNetwork().getLinkArray();
+//   // protected void layoutNodesAndLinks() {
+//   protected void layoutNodesAndLinksNotUsed() {
+//     JGoNetworkNode[] nodeArray = getNetwork().getNodeArray();
+//     JGoNetworkLink[] linkArray = getNetwork().getLinkArray();
 
 //     System.err.println( "nodeArray");
 //     for (int i = 0, n = nodeArray.length; i < n; i++) {
@@ -129,103 +130,103 @@ public class TokenNetworkLayout extends JGoLayeredDigraphAutoLayout {
 //       System.err.println( "  " + linkArray[i].getJGoObject());
 //     }
 
-    // layoutNodes();
-    // layoutLinks();
-    System.err.println( "views");
-    JGraph graph = new JGraph();
-    GraphModel graphModel = graph.getModel();
-    CellMapper mapper = new GraphLayoutCache( graphModel, graph);
-    CellView[] views = new CellView [ nodeArray.length ];
-    for (int i = 0, n = nodeArray.length; i < n; i++) {
-      views[i] = new TokenCellView( nodeArray[i], graph, mapper);
-      System.err.println( "TokenCellView " + views[i]);
-    }
-    ConnectionSet connectionSet = new ConnectionSet();
-    connectionSet.addConnections( views);
+//     // layoutNodes();
+//     // layoutLinks();
+//     System.err.println( "views");
+//     JGraph graph = new JGraph();
+//     GraphModel graphModel = graph.getModel();
+//     CellMapper mapper = new GraphLayoutCache( graphModel, graph);
+//     CellView[] views = new CellView [ nodeArray.length ];
+//     for (int i = 0, n = nodeArray.length; i < n; i++) {
+//       views[i] = new TokenCellView( nodeArray[i], graph, mapper);
+//       System.err.println( "TokenCellView " + views[i]);
+//     }
+//     ConnectionSet connectionSet = new ConnectionSet();
+//     connectionSet.addConnections( views);
 
-    System.err.println( "rootNodes");
-    // convert rootNodes & parentMap from TokenNode => TokenCellView
-    Object[] convertedRootNodes = new Object [rootNodes.length];
-    for (int i = 0, n = rootNodes.length; i < n; i++) {
-      TokenNode tokenNode = (TokenNode) rootNodes[i];
-      convertedRootNodes[i] =
-        new TokenCellView( mapTokenNodeToNetworkNode( tokenNode, nodeArray),
-                           graph, mapper);
-      System.err.println( "TokenCellView " + convertedRootNodes[i]);
-    }
-    System.err.println( "ParentMap");
-    ParentMap convertedParentMap = new ParentMap();
-    Iterator parentItr = parentMap.entries();
-    while (parentItr.hasNext()) {
-      ParentMap.Entry entry = (ParentMap.Entry) parentItr.next();
-      TokenNode childNode = (TokenNode) entry.getChild();
-      TokenNode parentNode = (TokenNode) entry.getParent();
-      TokenCellView childCell =
-        new TokenCellView( mapTokenNodeToNetworkNode( childNode, nodeArray), graph, mapper);
-      TokenCellView parentCell =
-        new TokenCellView( mapTokenNodeToNetworkNode( parentNode, nodeArray), graph, mapper);
-      System.err.println( "TokenCellView child " + childCell);
-      System.err.println( "TokenCellView parent " + parentCell);
-      convertedParentMap.addEntry(childCell, parentCell);
-    }
-    Map attributeMap = new HashMap();
-    AbstractUndoableEdit[] edits = new AbstractUndoableEdit [] { };
+//     System.err.println( "rootNodes");
+//     // convert rootNodes & parentMap from TokenNode => TokenCellView
+//     Object[] convertedRootNodes = new Object [rootNodes.length];
+//     for (int i = 0, n = rootNodes.length; i < n; i++) {
+//       TokenNode tokenNode = (TokenNode) rootNodes[i];
+//       convertedRootNodes[i] =
+//         new TokenCellView( mapTokenNodeToNetworkNode( tokenNode, nodeArray),
+//                            graph, mapper);
+//       System.err.println( "TokenCellView " + convertedRootNodes[i]);
+//     }
+//     System.err.println( "ParentMap");
+//     ParentMap convertedParentMap = new ParentMap();
+//     Iterator parentItr = parentMap.entries();
+//     while (parentItr.hasNext()) {
+//       ParentMap.Entry entry = (ParentMap.Entry) parentItr.next();
+//       TokenNode childNode = (TokenNode) entry.getChild();
+//       TokenNode parentNode = (TokenNode) entry.getParent();
+//       TokenCellView childCell =
+//         new TokenCellView( mapTokenNodeToNetworkNode( childNode, nodeArray), graph, mapper);
+//       TokenCellView parentCell =
+//         new TokenCellView( mapTokenNodeToNetworkNode( parentNode, nodeArray), graph, mapper);
+//       System.err.println( "TokenCellView child " + childCell);
+//       System.err.println( "TokenCellView parent " + parentCell);
+//       convertedParentMap.addEntry(childCell, parentCell);
+//     }
+//     Map attributeMap = new HashMap();
+//     AbstractUndoableEdit[] edits = new AbstractUndoableEdit [] { };
 
-    graphModel.insert( convertedRootNodes, attributeMap, connectionSet,
-                       convertedParentMap, edits);
+//     graphModel.insert( convertedRootNodes, attributeMap, connectionSet,
+//                        convertedParentMap, edits);
 
-    boolean applyToAll = true;
-    Properties properties = new Properties();
-    properties.setProperty( KEY_WIDTH, "50.0");
-    properties.setProperty( KEY_HEIGHT, "50.0");
+//     boolean applyToAll = true;
+//     Properties properties = new Properties();
+//     properties.setProperty( KEY_WIDTH, "50.0");
+//     properties.setProperty( KEY_HEIGHT, "50.0");
 
-    new RadialTreeLayoutAlgorithm().perform( graph, applyToAll, properties);
-  } // end layoutNodesAndLinks
+//     new RadialTreeLayoutAlgorithm().perform( graph, applyToAll, properties);
+//   } // end layoutNodesAndLinks
 
-  private JGoNetworkNode mapTokenNodeToNetworkNode( TokenNode tokenNode,
-                                                    JGoNetworkNode[] nodeArray) {
-    for (int j = 0, m = nodeArray.length; j < m; j++) {
-      if (((TokenNode) ((JGoNetworkNode) nodeArray[j]).getJGoObject()).
-          getToken().getId().equals( tokenNode.getToken().getId())) {
-        return (JGoNetworkNode) nodeArray[j];
-      }
-    }
-    System.err.println( "TokenNetworkLayout.mapTokenNodeToNetworkNode " +
-                        "token Node " + tokenNode.getToken().getId() +
-                        " not in network");
-    return null;
-  }
+//   private JGoNetworkNode mapTokenNodeToNetworkNode( TokenNode tokenNode,
+//                                                     JGoNetworkNode[] nodeArray) {
+//     for (int j = 0, m = nodeArray.length; j < m; j++) {
+//       if (((TokenNode) ((JGoNetworkNode) nodeArray[j]).getJGoObject()).
+//           getToken().getId().equals( tokenNode.getToken().getId())) {
+//         return (JGoNetworkNode) nodeArray[j];
+//       }
+//     }
+//     System.err.println( "TokenNetworkLayout.mapTokenNodeToNetworkNode " +
+//                         "token Node " + tokenNode.getToken().getId() +
+//                         " not in network");
+//     return null;
+//   }
 
 
     
-  public class TokenCellView extends AbstractCellView {
+//   public class TokenCellView extends AbstractCellView {
 
-    private JGoObject jGoObject;
+//     private JGoObject jGoObject;
 
-    public TokenCellView( JGoNetworkNode networkNode, JGraph graph, CellMapper mapper) {
-      super( networkNode, graph, mapper);
-      System.err.println( "TokenCellView.networkNode " + networkNode);
-      jGoObject = networkNode.getJGoObject();
-      System.err.println( "TokenCellView.jGoObject " + networkNode.getJGoObject());
-    }
+//     public TokenCellView( JGoNetworkNode networkNode, JGraph graph, CellMapper mapper) {
+//       super( networkNode, graph, mapper);
+//       System.err.println( "TokenCellView.networkNode " + networkNode);
+//       jGoObject = networkNode.getJGoObject();
+//       System.err.println( "TokenCellView.jGoObject " + networkNode.getJGoObject());
+//     }
 
-    public Rectangle2D getBounds() {
-      return (Rectangle2D) jGoObject.getBoundingRect();
-    }
+//     public Rectangle2D getBounds() {
+//       return (Rectangle2D) jGoObject.getBoundingRect();
+//     }
 
-    public CellHandle getHandle( GraphContext context) {
-      return null;
-    }
+//     public CellHandle getHandle( GraphContext context) {
+//       return null;
+//     }
 
-    public CellViewRenderer getRenderer() {
-      return null;
-    }
+//     public CellViewRenderer getRenderer() {
+//       return null;
+//     }
 
-    public JGoObject getJGoObject() {
-      return jGoObject;
-    }
+//     public JGoObject getJGoObject() {
+//       return jGoObject;
+//     }
 
-  } // end class TokenCellView
+//   } // end class TokenCellView
 
 
 
