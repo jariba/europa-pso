c77a3ce8404fc5950ff344c94230a3e940ff70b7
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2004-08-26 23:00:42 +0000

    moved bulk of test language

--- PlanWorks/trunk/java/src/gov/nasa/arc/planworks/dbg/testLang/Domain.java
+++ PlanWorks/trunk/java/src/gov/nasa/arc/planworks/dbg/testLang/Domain.java
@@ -1,16 +1,5 @@
 package gov.nasa.arc.planworks.dbg.testLang;
 
-/*NOTE: the contract of Comparable is abused slightly by Domain, since there are
-	different kinds of equality.  compareTo() return values are as follows:
-	-2 : <=
-	-1 : <
-	 0 : =
-	 1 : >
-	 2 : >=
-   3 : in
-   4 : out
-*/
-
 public interface Domain {
 	public Comparable getFirst();
 	public Comparable getLast();
