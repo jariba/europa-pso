4df186b153e6b781177cc755989699552ca82cca
miatauro <miatauro@23e1bc29-d954-0410-9766-47de34158ba0>
2005-02-15 23:04:46 +0000

    added ability to change MySQL port number

--- PlanWorks/trunk/cleanDb.sh
+++ PlanWorks/trunk/cleanDb.sh
@@ -2,10 +2,10 @@
 
 echo $1  $2  $3
 
-eval "$1/mysqld --basedir=$2 --skip-bdb --datadir=$3 --log=$4 --log-error=$5 --skip-symlink --socket=$6 --tmpdir=$7 &"
+eval "$1/mysqld --basedir=$2 --skip-bdb --datadir=$3 --log=$4 --log-error=$5 --skip-symlink --socket=$6 --tmpdir=$7 --port=$8 &"
 
 sleep 1
 
-eval "$1/mysql --user=root --socket=$6 < cleandb"
+eval "$1/mysql --user=root --socket=$6 --port=$8 < cleandb"
 
-eval "$1/mysqladmin --user=root --socket=$6 shutdown"
+eval "$1/mysqladmin --user=root --socket=$6 --port=$8 shutdown"
