6913ee4d1337bf856ea5ca217f4b9d91d69d0889
javier <javier@23e1bc29-d954-0410-9766-47de34158ba0>
2006-11-06 22:23:52 +0000

    PSUI is a subproject of PLanWorks for now. it will be integrated later.

---
+++ PlanWorks/trunk/PSUI/src/org/ops/test/ui/chart/PSCrewResourceChartModel.java
@@ -0,0 +1,22 @@
+package org.ops.test.ui.chart;
+
+import org.ops.ui.chart.PSResourceChartModel;
+
+import dsa.ResourceProfile;
+
+public class PSCrewResourceChartModel 
+    implements PSResourceChartModel 
+{
+
+	public ResourceProfile getCapacity() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResourceProfile getUsage() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
