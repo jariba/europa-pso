9f8691f138ea092b5b0efb7d73da5f2e63f94c26
javier <javier@23e1bc29-d954-0410-9766-47de34158ba0>
2007-01-24 00:48:28 +0000

    checkpoint. migrating to PSEngine from DSA.

--- PlanWorks/trunk/PSUI/src/org/ops/ui/mouse/ActionDetailsPanel.java
+++ PlanWorks/trunk/PSUI/src/org/ops/ui/mouse/ActionDetailsPanel.java
@@ -5,30 +5,69 @@ import dsa.Action;
 import dsa.DSA;
 import dsa.Parameter;
 
+import psengine.PSEngine;
+import psengine.PSToken;
+import psengine.PSVariable;
+import psengine.PSVariableList;
+
 public class ActionDetailsPanel 
     extends MouseListenerPanel 
 {
 	private static final long serialVersionUID = 7779941401503562818L;
 
-	protected DSA dsa_;
+	protected DSA dsa_ = null;
+	protected PSEngine psengine_ = null;
 	
     public ActionDetailsPanel(DSA dsa)
     {
     	dsa_ = dsa;
     }
 	
+    public ActionDetailsPanel(PSEngine pse)
+    {
+    	psengine_ = pse;
+    }
+	
 	@Override
 	public void mouseMoved(Object key) 
 	{
-		if (key == null) {
+		if (key == null) 
 			text_.setText("");
-		}
-		else {
-		    Action act = dsa_.getAction((Integer)key);
-		    text_.setText(actionDetails(act));
-		}
+		else 
+		    text_.setText(getDetails((Integer)key));
+	}
+	
+	protected String getDetails(Integer key)
+	{
+		if (dsa_ != null) 
+			return actionDetails(dsa_.getAction(key)); 
+			
+		
+		return tokenDetails(psengine_.getTokenByKey(key));
 	}
 		
+	protected String tokenDetails(PSToken t)
+	{
+		StringBuffer buf = new StringBuffer();
+
+        buf.append("ID     : ").append(t.getKey()).append("\n")
+           .append("Name   : ").append(t.getName()).append("\n")
+           .append("Start  : ").append(t.getParameter("start")).append("\n")
+           .append("Finish : ").append(t.getParameter("end")).append("\n")
+        ;
+        
+        if (t.getParameters().size() > 0) {
+            buf.append("Parameters:").append("\n");
+            PSVariableList parameters = t.getParameters();
+            for (int i = 0; i< parameters.size(); i++) {
+            	PSVariable p = parameters.get(i);
+                buf.append(p.getName()).append(" : ").append(p.toString()).append("\n");
+            }
+        }
+           
+		return buf.toString();
+	}
+	
 	protected String actionDetails(Action act)
 	{
 		StringBuffer buf = new StringBuffer();
--- PlanWorks/trunk/PSUI/src/org/ops/ui/mouse/ActionViolationsPanel.java
+++ PlanWorks/trunk/PSUI/src/org/ops/ui/mouse/ActionViolationsPanel.java
@@ -5,32 +5,54 @@ import dsa.DSA;
 import dsa.Action;
 import dsa.Violation;
 
+import psengine.PSEngine;
+import psengine.PSToken;
+
 public class ActionViolationsPanel 
     extends MouseListenerPanel 
 {
 	private static final long serialVersionUID = 7462693069863073725L;
 
-	protected DSA dsa_;
+	protected DSA dsa_ = null;
+	protected PSEngine psengine_ = null;
 	
     public ActionViolationsPanel(DSA dsa)
     {
     	dsa_ = dsa;
     }
 	
+    public ActionViolationsPanel(PSEngine pse)
+    {
+    	psengine_ = pse;
+    }
+	
 	@Override
 	public void mouseMoved(Object key) 
 	{
-		if (key == null) {
+		if (key == null) 
 			text_.setText("");
-		}
-		else {
-		    Action act = dsa_.getAction((Integer)key);
-		    text_.setText(violationsToText(act.getViolations()));
-		}
+		else 
+		    text_.setText(getViolations((Integer)key));
 	}
 		
-	protected String violationsToText(List<Violation> violations)
+	protected String getViolations(Integer key)
 	{
+		if (dsa_ != null) 
+			return actionViolations(dsa_.getAction(key)); 
+			
+		
+		return tokenViolations(psengine_.getTokenByKey(key));
+	}
+		
+	protected String tokenViolations(PSToken t)
+	{
+		return t.getViolationExpl();
+	}
+	
+	protected String actionViolations(Action a)
+	{
+		List<Violation> violations = a.getViolations();
+		
 		if (violations==null || violations.size()==0)
 			return "No Violations";
 		
