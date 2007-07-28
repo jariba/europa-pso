324e86cc7ccde8e3df38a449fe71ac521265e715
javier <javier@23e1bc29-d954-0410-9766-47de34158ba0>
2007-07-28 00:27:44 +0000

    checkpoint. adding violation reporting for temporal constraints

--- PlanWorks/trunk/PSUI/test/ResourceViolations/ResourceViolations-Main.cc
+++ PlanWorks/trunk/PSUI/test/ResourceViolations/ResourceViolations-Main.cc
@@ -58,24 +58,30 @@ void testViolations(PSEngine& psengine)
 	
 	PSObject* act_obj1 = psengine.getObjectsByType("Activity").get(0);
 	PSObject* act_obj2 = psengine.getObjectsByType("Activity").get(1);
+	PSObject* act_obj3 = psengine.getObjectsByType("Activity").get(2);
 
 	PSToken* act1 = act_obj1->getTokens().get(0);
 	PSToken* act2 = act_obj2->getTokens().get(0);	
+	PSToken* act3 = act_obj3->getTokens().get(0);	
 
 	PSVariable* s1 = act1->getParameter("start");
 	PSVariable* s2 = act2->getParameter("start");
+	PSVariable* s3 = act3->getParameter("start");
 
-	PSVarValue five = PSVarValue::getInstance(5);
-	PSVarValue eleven = PSVarValue::getInstance(11);
+	PSVarValue vv5 = PSVarValue::getInstance(5);
+	PSVarValue vv11 = PSVarValue::getInstance(11);
+	PSVarValue vv18 = PSVarValue::getInstance(18);
+	PSVarValue vv20 = PSVarValue::getInstance(20);
 
 	// Cause Violation
-	s1->specifyValue(five);
-	s2->specifyValue(five);
+	s1->specifyValue(vv5);
+	s2->specifyValue(vv11);
+	s2->specifyValue(vv20);
 	debugMsg("testViolations",psengine.getViolation());
 	debugMsg("testViolations",psengine.getViolationExpl());
 
 	// Remove Violation
-	s2->specifyValue(eleven);
+	s3->specifyValue(vv18);
 	debugMsg("testViolations",psengine.getViolation());
 	debugMsg("testViolations",psengine.getViolationExpl());
 }
