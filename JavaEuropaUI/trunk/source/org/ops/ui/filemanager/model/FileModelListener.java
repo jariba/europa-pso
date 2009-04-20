85b88d39cbee9d01814e4f514ce3efbd33ba9655
kichkaylo <kichkaylo@23e1bc29-d954-0410-9766-47de34158ba0>
2009-04-20 04:33:31 +0000

    First real commit: only file manager and plugin activator for now

---
+++ JavaEuropaUI/trunk/source/org/ops/ui/filemanager/model/FileModelListener.java
@@ -0,0 +1,10 @@
+package org.ops.ui.filemanager.model;
+
+/**
+ * Listener to the file model.
+ * 
+ * @author Tatiana Kichkaylo
+ */
+public interface FileModelListener {
+	public void databaseReloaded();
+}
