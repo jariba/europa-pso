e557861f011c37ab12f711ae820091e05e990bff
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2009-06-11 01:12:44 +0000

    moving EUROPA-2.2 tag

--- PLASMA/tags/EUROPA-2.2/bin/.makeproject/Template.i
+++
@@ -1,7 +0,0 @@
-%module %%Project%%
-%include "std_string.i"
-
-%{
-  #include "%%Project%%CustomCode.hh"
-  #include "Module%%Project%%.hh"
-%}
--- PLASMA/tags/EUROPA-2.2/bin/.makeproject/TemplateCustomCode.hh
+++
@@ -1,6 +0,0 @@
-#ifndef _H_%%Project%%CustomCode
-#define _H_%%Project%%CustomCode
-
-// Put any C++ project-specific custom code here
-
-#endif
