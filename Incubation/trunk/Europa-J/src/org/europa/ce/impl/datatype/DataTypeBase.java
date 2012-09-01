8405a8396af33208622363b7a7b9fe2f247ed867
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2012-09-01 02:17:26 +0000

    checkpoint. fleshing out domains.

--- Incubation/trunk/Europa-J/src/org/europa/ce/impl/datatype/DataTypeBase.java
+++ Incubation/trunk/Europa-J/src/org/europa/ce/impl/datatype/DataTypeBase.java
@@ -4,26 +4,33 @@ import org.europa.ce.CVariable;
 import org.europa.ce.ConstraintEngine;
 import org.europa.ce.DataType;
 import org.europa.ce.Domain;
+import org.europa.ce.impl.CVariableImpl;
 
 public abstract class DataTypeBase 
 	implements DataType 
 {
 	protected String name_;
+	protected Domain baseDomain_;
 	
-	public DataTypeBase(String name)
+	public DataTypeBase(String name, Domain baseDomain)
 	{
 		name_ = name;
+		baseDomain_ = baseDomain;
 	}
 	
 	@Override public String getName() { return name_; }
 
 	@Override
-	public CVariable createVariable(ConstraintEngine constraintEngine,
-			Domain restrictedBaseDomain, boolean internal,
-			boolean canBeSpecified, String name, Object parent, int index) 
+	public CVariable createVariable(
+			ConstraintEngine constraintEngine,
+			String name, 
+			Object parent, 
+			int index,
+			Domain restrictedBaseDomain, 
+			boolean internal,
+			boolean canBeSpecified) 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		Domain baseDomain = (restrictedBaseDomain==null ? baseDomain_ : restrictedBaseDomain);
+		return new CVariableImpl(constraintEngine,name,this,baseDomain,parent,index,internal,canBeSpecified);
 	}
-
 }
