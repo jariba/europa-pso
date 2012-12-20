86e2b7b28dc447534e28b7b90bea22bda2235b0e
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2012-12-20 21:15:03 +0000

    checkpoint. fleshing out CommandService.

--- Incubation/trunk/psim/src/org/space/oss/psim/CommandService.java
+++ Incubation/trunk/psim/src/org/space/oss/psim/CommandService.java
@@ -2,4 +2,11 @@ package org.space.oss.psim;
 
 public interface CommandService extends PSimService
 {
+	// Methods to manage event queue
+	public void queueCommand(Command c);
+	public void removeCommand(Integer commandID);
+	public boolean sendQueuedCommand(int retries,boolean discardOnFail);
+	public void sendAllQueuedCommands(int retries,boolean discardOnFail);
+	public int getCommandQueueSize();
+	public void clearCommandQueue();
 }
--- Incubation/trunk/psim/src/org/space/oss/psim/command/CommandServiceImpl.java
+++ Incubation/trunk/psim/src/org/space/oss/psim/command/CommandServiceImpl.java
@@ -1,6 +1,11 @@
 package org.space.oss.psim.command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.space.oss.psim.CommChannel;
+import org.space.oss.psim.Command;
 import org.space.oss.psim.CommandService;
 import org.space.oss.psim.Config;
 
@@ -9,9 +14,12 @@ public class CommandServiceImpl implements CommandService
 {
 	private static Logger LOG = Logger.getLogger(CommandServiceImpl.class);
 
+	protected List<Command> commands_;
+	
 	@Override
 	public void init(Config cfg) 
 	{
+		commands_ = new ArrayList<Command>();
 		LOG.info("Initialized CommandService");
 	}
 
@@ -19,5 +27,78 @@ public class CommandServiceImpl implements CommandService
 	public void shutdown() 
 	{
 		LOG.info("Shut down CommandService");
+	}
+
+	@Override
+	public void queueCommand(Command c) 
+	{
+		commands_.add(0,c);
+	}
+
+	@Override
+	public void removeCommand(Integer commandID) 
+	{
+		for (Command c : commands_) {
+			if (c.getID().equals(commandID)) {
+				commands_.remove(c);
+				return;
+			}
+		}
+		
+		throw new RuntimeException("Command "+commandID+" not found for removal");
+	}
+
+	@Override
+	public boolean sendQueuedCommand(int retries, boolean discardOnFail) 
+	{
+		assert commands_.size()>0;
+		
+		Command c = commands_.get(0);
+		CommChannel comm = getCommChannel(c);
+		
+		for (int cnt = 0; cnt < retries; cnt++) {
+			// TODO: create comm packet?
+			boolean ok = comm.sendMessage(c);
+			if (ok) {
+				LOG.debug(c + " sent successfully");
+				return true;
+			}
+		}
+		
+		LOG.debug(c + " failed to be sent");
+		if (discardOnFail) {
+			commands_.remove(c);
+			LOG.debug(c + "discarded");
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void sendAllQueuedCommands(int retries, boolean discardOnFail) 
+	{
+		while (getCommandQueueSize() > 0) {
+			boolean ok = sendQueuedCommand(retries,discardOnFail);
+			if (!ok && !discardOnFail)
+				return;
+		}
+	}
+
+	@Override
+	public int getCommandQueueSize() 
+	{
+		return commands_.size();
+	}
+
+	@Override
+	public void clearCommandQueue() 
+	{
+		commands_.clear();
+	}
+	
+	protected CommChannel getCommChannel(Command c)
+	{
+		// TODO: implement through factory
+		return null;
 	}
 }
