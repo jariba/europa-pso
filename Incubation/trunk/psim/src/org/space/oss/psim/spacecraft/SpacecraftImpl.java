4f2200800a4fdc1d995d218644a255c577d82a00
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2012-12-22 00:51:19 +0000

    checkpoint. fleshing out basic services in PSim.

--- Incubation/trunk/psim/src/org/space/oss/psim/spacecraft/SpacecraftImpl.java
+++ Incubation/trunk/psim/src/org/space/oss/psim/spacecraft/SpacecraftImpl.java
@@ -1,18 +1,24 @@
 package org.space.oss.psim.spacecraft;
 
 import org.apache.log4j.Logger;
+import org.space.oss.psim.CommChannel;
 import org.space.oss.psim.Message;
+import org.space.oss.psim.PSim;
 import org.space.oss.psim.Spacecraft;
+import org.space.oss.psim.comms.CommChannelImpl;
+import org.space.oss.psim.comms.MessageImpl;
 
 public class SpacecraftImpl implements Spacecraft 
 {
 	private static Logger LOG = Logger.getLogger(SpacecraftImpl.class);
 	
+	protected PSim psim_;
 	protected String id_;
 	
-	public SpacecraftImpl(String id)
+	public SpacecraftImpl(String id, PSim psim)
 	{
 		id_ = id;
+		psim_ = psim;
 	}
 	
 	@Override
@@ -22,5 +28,14 @@ public class SpacecraftImpl implements Spacecraft
 	public void receiveMessage(Message message) 
 	{
 		LOG.debug(getID()+ " received: "+message);
+		String dest = message.getSender();
+		CommChannel cc = getCommChannel(dest);
+		cc.sendMessage(new MessageImpl(getID(),dest,"OK- "+getID()+" processed "+message));
+	}
+	
+	protected CommChannel getCommChannel(String dest)
+	{
+		// TODO: implement through factory
+		return new CommChannelImpl(psim_,getID(),dest);
 	}
 }
