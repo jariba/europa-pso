5cc635fa2b87a3ecdca159494df6dfc0f73e7f1f
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2013-04-11 01:18:17 +0000

    PSim: added ability to load command dictionary from a JSON file.

--- Incubation/trunk/psim/src/org/space/oss/psim/CommandDescriptor.java
+++ Incubation/trunk/psim/src/org/space/oss/psim/CommandDescriptor.java
@@ -4,19 +4,20 @@ package org.space.oss.psim;
 public class CommandDescriptor 
 {
     protected String name;
-	protected String category;
-    protected CommandArg args[];
+    protected String code;
+	protected String subsystem;
+    protected CommandArg parameters[];
 	
 	public CommandDescriptor(String category, String name, CommandArg args[])
 	{
-		this.category = category;
+		this.subsystem = category;
 		this.name = name;
-		this.args = args;
+		this.parameters = args;
 	}
 	
-	public String getCategory() { return category; }
+	public String getCategory() { return subsystem; }
 	public String getName() { return name; }
-	public CommandArg[] getArgs() { return args; }
+	public CommandArg[] getArgs() { return parameters; }
 	
 	public String toString() { return getName(); }
 }
