fde9e0e67a2cb27a856958dc55169123c64a7835
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2013-08-14 20:32:28 +0000

    psim-scala: added swing desktop with beanshell console.

--- Incubation/trunk/psim-scala/src/main/scala/org/space/oss/psim/PSimServer.scala
+++ Incubation/trunk/psim-scala/src/main/scala/org/space/oss/psim/PSimServer.scala
@@ -1,18 +1,35 @@
 package org.space.oss.psim
 
-object PSimServer 
+import org.space.oss.psim.ui.PSimDesktop
+
+class PSimServer
 {
   var psim: PSim = null
+
+  def init(cfg:Config) {
+    psim = new PSimImpl
+    psim.init(cfg)
+    //println(psim.toString)    
+  }
+}
+
+object PSimServer 
+{
+  var instance:PSimServer = _
   
   def main(args: Array[String]) {
     val cfg = new ConfigByMap
-    psim = new PSimImpl
-    psim.init(cfg)
-    println(psim.toString)
-    
-    val mgr = new PSimEventManager(psim)
+    instance = new PSimServer
+    instance.init(cfg)
+    val desktop = new PSimDesktop(instance,cfg)
+    desktop.run()
+    //runSimulation()
+  }
+  
+  def runSimulation() {
+    val mgr = new PSimEventManager(instance.psim)
     mgr.init()
     mgr ! Start
-    mgr.start()
+    mgr.start()    
   }
 }
