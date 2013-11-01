095d71e91156c4f779f1e629b69597f9a3a69db2
javier.barreiro <javier.barreiro@23e1bc29-d954-0410-9766-47de34158ba0>
2013-11-01 18:39:41 +0000

    psim-scala: small debug printout change.

--- Incubation/trunk/psim-scala/src/main/scala/org/space/oss/psim/spacecraft/SpacecraftBase.scala
+++ Incubation/trunk/psim-scala/src/main/scala/org/space/oss/psim/spacecraft/SpacecraftBase.scala
@@ -64,7 +64,7 @@ class SpacecraftBase(id:String) extends Spacecraft
 	 
 	 def logCommand(c:AnyRef) {
 	   commandTrace = commandTrace :+ c
-	   //println("Executed Command: "+c)
+	   //println(this.getID+" Executed Command: "+c)
 	   this.notifyEvent(Spacecraft.EXECUTED_COMMAND,ExecutedCommand(c))
 	 }
 	 
